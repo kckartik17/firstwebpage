2f1ecf2eaeb7811dd395c011be7ffb56d2c5e172
Kartik Singh Chauhan <lkchauhan.725@gmail.com>
2017-12-30 21:55:04 +0530

    made changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/kartik.jpg') {
-      myImage.setAttribute ('src','images/firefox.jpg');
-    } else {
+    if(mySrc === 'images/me.jpg') {
       myImage.setAttribute ('src','images/kartik.jpg');
+    } else {
+      myImage.setAttribute ('src','images/me.jpg');
     }
 }
 
@@ -14,13 +14,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Kartik is cool, ' + myName;
+  myHeading.textContent = 'Hi '+ myName + ' ! Kartik is cool';
+
 }
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Kartik is cool, ' + storedName;
+  myHeading.textContent = 'Hi '+ storedName + ' ! Kartik is cool';
 }
 myButton.onclick = function() {
   setUserName();
