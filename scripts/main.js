d901e651a69a7d11b89a2b0ccee67452acef9f97
Kartik Chauhan <32371826+chauhankartik1708@users.noreply.github.com>
2017-12-30 16:12:02 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,13 +14,13 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Kartik is cool, ' + myName;
 }
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Kartik is cool, ' + storedName;
 }
 myButton.onclick = function() {
   setUserName();
